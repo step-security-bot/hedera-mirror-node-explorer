a7b5663e86598af4e217db22e5e0e7a90a8267b6
Eric Le Ponner <eric.leponner@icloud.com>
2023-02-06 17:08:56 +0100

    Removed hard-coded compiler version from RegistrationController (and uses latest in place).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/components/registration/SolcIndexLoader.ts
+++ src/components/registration/SolcIndexLoader.ts
@@ -24,6 +24,50 @@ import axios, {AxiosResponse} from "axios";
 
 export class SolcIndexLoader extends EntityLoader<SolcIndex> {
 
+
+    //
+    // Public
+    //
+
+    public fetchLongVersion(version: string): string|null {
+
+        /*
+            {
+              "path": "soljson-v0.8.18+commit.87f61d96.js",
+              "version": "0.8.18",
+              "build": "commit.87f61d96",
+              "longVersion": "0.8.18+commit.87f61d96",
+              "keccak256": "0x9a8fa4183ef95496045189b80dfb39f745db89a903b398e40131f500953e5d57",
+              "sha256": "0xd82bdcba2c386d60b33aca148a9cfdf097551f68c5e45d8ec01aebbafacf5075",
+              "urls": [
+                "bzzr://338117c2130fcb6bce3006330712b6e7ee99875b56ce4bb6182312f76e4a6bac",
+                "dweb:/ipfs/QmcKzrqRBy7PeFQxzJDs1AZZzNHKaKbJces6zUDysXZofJ"
+              ]
+            },
+
+         */
+
+        let result: string|null
+        if (this.entity.value !== null && version in this.entity.value.releases) {
+            result = null
+            const path = this.entity.value.releases[version]
+            for (const b of this.entity.value.builds) {
+                if (b.path == path) {
+                    result = b.longVersion
+                    break
+                }
+            }
+        } else {
+            result = null
+        }
+
+        return result
+    }
+
+    //
+    // EntityLoader
+    //
+
     protected async load(): Promise<AxiosResponse<SolcIndex> | null> {
         return axios.get("https://binaries.soliditylang.org/bin/list.json")
     }
