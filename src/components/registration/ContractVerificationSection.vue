0a78acacb3421d0ced724f4846bafacac6b223c6
Eric Le Ponner <eric.leponner@icloud.com>
2023-04-25 17:27:59 +0200

    Added ContractVerificationSection and ContractVerificationStatus vues (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

---
+++ src/components/registration/ContractVerificationSection.vue
@@ -0,0 +1,118 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+
+  <DashboardCard>
+    <template v-slot:title>
+      <span class="h-is-secondary-title">Source Code Verification</span>
+      <span v-if="contractName" class="icon has-text-success ml-2"><i class="far fa-check-circle"></i></span>
+    </template>
+
+    <template v-slot:control>
+      <div v-if="sourcifyURL" id="showSource" class="is-inline-block ml-3">
+        <a :href="sourcifyURL" target="_blank">View in Sourcify</a>
+      </div>
+      <span v-else-if="compiling" class="icon mr-2" style="font-size: 18px">
+        <i class="fa fa-circle-notch fa-spin"></i>
+      </span>
+    </template>
+
+    <template v-slot:content>
+      <Property id="verificationStatus" :full-width="true">
+        <template v-slot:name>Verification Status</template>
+        <template v-slot:value>
+          <span v-if="fullMatch"><ContractVerificationStatus :full-match="fullMatch"/></span>
+<!--          <span v-else-if="compiling">Verifying contract…</span>-->
+          <span v-else>Not yet verified</span>
+        </template>
+      </Property>
+
+      <Property id="contractName" :full-width="true">
+        <template v-slot:name>Contract Name</template>
+        <template v-slot:value>
+          <StringValue :string-value="contractName"/>
+        </template>
+      </Property>
+
+<!--      <Property id="sourceFileName" :full-width="true">-->
+<!--        <template v-slot:name>Contract Source File</template>-->
+<!--        <template v-slot:value>-->
+<!--          <a  v-if="sourceFileURL" :href="sourceFileURL" target="_blank">{{ sourceFileName }}</a>-->
+<!--          <span v-else class="has-text-grey">None</span>-->
+<!--        </template>-->
+<!--      </Property>-->
+
+    </template>
+  </DashboardCard>
+
+</template>
+
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+
+<script lang="ts">
+
+import {computed, defineComponent, PropType} from 'vue';
+import {ContractAnalyzer} from "@/utils/ContractAnalyzer";
+import DashboardCard from "@/components/DashboardCard.vue";
+import Property from "@/components/Property.vue";
+import StringValue from "@/components/values/StringValue.vue";
+import ContractVerificationStatus from "@/components/registration/ContractVerificationStatus.vue";
+
+export default defineComponent({
+    name: 'ContractVerificationSection',
+    components: {ContractVerificationStatus, StringValue, Property, DashboardCard},
+    props: {
+        contractAnalyzer: {
+            type: Object as PropType<ContractAnalyzer>,
+            required: true
+        }
+    },
+
+    setup(props) {
+
+        const compiling = computed(() => false)
+
+        return {
+            contractId: props.contractAnalyzer.contractId,
+            contractName: props.contractAnalyzer.contractName,
+            sourceFileName: props.contractAnalyzer.sourceFileName,
+            fullMatch: props.contractAnalyzer.fullMatch,
+            sourcifyURL: props.contractAnalyzer.sourcifyURL,
+            compiling
+        }
+    }
+});
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
---
+++ src/components/registration/ContractVerificationStatus.vue
@@ -0,0 +1,81 @@
+<!--
+  -
+  - Hedera Mirror Node Explorer
+  -
+  - Copyright (C) 2021 - 2022 Hedera Hashgraph, LLC
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -      http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -
+  -->
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                     TEMPLATE                                                    -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<template>
+  <span v-if="isFullMatch">
+    Full Match
+    <span class="has-text-grey">
+      <span class="ml-2">(see</span>
+      <a class="ml-1" href="https://docs.hedera.com/hedera/">docs.hedera.com</a>
+      <span>)</span>
+    </span>
+
+  </span>
+  <span v-else-if="isPartialMatch">
+    Partial Match
+    <span class="has-text-grey">
+      <span class="ml-2">(see</span>
+      <a class="ml-1 has-text-grey" href="https://docs.hedera.com/hedera/">docs.hedera.com</a>
+      <span>)</span>
+    </span>
+  </span>
+  <span v-else>Source code does not match contract bytecode</span>
+</template>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                      SCRIPT                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<script lang="ts">
+
+import {computed, defineComponent} from 'vue';
+
+export default defineComponent({
+    name: 'ContractVerificationStatus',
+    props: {
+        fullMatch: Boolean
+    },
+
+    setup(props) {
+
+        const isFullMatch = computed(
+            () => typeof props.fullMatch != "undefined" && props.fullMatch)
+
+        const isPartialMatch = computed(
+            () => typeof props.fullMatch != "undefined" && !props.fullMatch)
+
+        return {
+            isFullMatch,
+            isPartialMatch
+        }
+    }
+});
+
+</script>
+
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+<!--                                                       STYLE                                                     -->
+<!-- --------------------------------------------------------------------------------------------------------------- -->
+
+<style/>
