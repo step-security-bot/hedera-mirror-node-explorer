55503d2a79fe386982f3df36a5a4249390858d47
Eric Le Ponner <eric.leponner@icloud.com>
2024-04-09 09:59:45 +0200

    Added forceLoad and makeAutoRefreshLookup() to SingletonCache (to be used later).
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/utils/cache/base/SingletonCache.ts
+++ src/utils/cache/base/SingletonCache.ts
@@ -24,24 +24,36 @@ import {Lookup} from "@/utils/cache/base/Lookup";
 export class SingletonCache<E> {
 
     protected promise: Promise<E>|null = null
+    private time: number = -1
 
     //
     // Public
     //
 
-    public async lookup(): Promise<E> {
-        if (this.promise === null) {
-            this.promise = this.load()
+    public async lookup(forceLoad = false): Promise<E> {
+        let result: Promise<E>
+
+        const fresh = (Date.now() - this.time) < 500 // ms
+        if (this.promise !== null && (fresh || !forceLoad)) {
+            result = this.promise
+        } else {
+            result = this.load()
+            this.promise = result
         }
-        return this.promise
+        return result
     }
 
     public clear(): void {
         this.promise = null
+        this.time = - 1
     }
 
     public makeLookup(): SingletonLookup<E> {
         return new SingletonLookup(this)
+    }
+
+    public makeAutoRefreshLookup(refreshPeriod: number): SingletonAutoRefreshLookup<E> {
+        return new SingletonAutoRefreshLookup(this, refreshPeriod)
     }
 
 
@@ -72,5 +84,60 @@ export class SingletonLookup<E> implements Lookup<E>{
 
     public unmount(): void {
         this.entity.value = null
+    }
+}
+
+export class SingletonAutoRefreshLookup<E> implements Lookup<E> {
+
+    public readonly entity: Ref<E|null> = ref(null)
+    public readonly refreshPeriod: number // milliseconds
+    private readonly cache: SingletonCache<E>
+    private mounted = false
+    private timeout: number = -1
+
+    constructor(cache: SingletonCache<E>, refreshPeriod: number) {
+        this.cache = cache
+        this.refreshPeriod = refreshPeriod
+    }
+
+    public mount(): void {
+        this.mounted = true
+        this.startRefresh()
+    }
+
+    public unmount(): void {
+        this.stopRefresh()
+        this.mounted = false
+    }
+
+    //
+    // Private
+    //
+
+    private startRefresh() {
+        this.cache.lookup(true)
+            .then((s: E) => {
+                this.lookupDidComplete(s)
+            })
+            .catch(() => {
+                this.lookupDidComplete(null)
+            })
+    }
+
+    private stopRefresh() {
+        if (this.timeout !== -1) {
+            window.clearTimeout(this.timeout)
+            this.timeout = -1
+        }
+        this.entity.value = null
+    }
+
+    private lookupDidComplete(e: E|null) {
+        if (this.mounted) {
+            this.entity.value = e
+            this.timeout = window.setTimeout(() => {
+                this.startRefresh()
+            }, this.refreshPeriod)
+        } // else we silently exit because everything is already setup by unmount()
     }
 }
